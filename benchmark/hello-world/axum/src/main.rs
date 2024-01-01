6db8f59187164805050734d20e0beef89b9e8bf4
M220 <Mohammad2x3x@gmail.com>
2024-01-01 21:44:53 +0330

    fix some axum stuff

--- benchmark/hello-world/axum/src/main.rs
+++ benchmark/hello-world/axum/src/main.rs
@@ -4,6 +4,8 @@ use axum::{routing::get, Router};
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
